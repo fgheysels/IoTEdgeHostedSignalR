7ec0d3515594b25a9109f3c948e0075b7b53f644
Frederik Gheysels <frederik.gheysels@telenet.be>
2021-04-09 17:37:47 +0200

    Add TelemetryClient app
    The TelemetryClient app subscribes to the signalr host for getting real time telemetry updates

---
+++ TelemetryClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace TelemetryClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static SignalRSettings GetConfiguration()
+        {
+            var configuration = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                                .AddJsonFile("appsettings.json", false)
+                                .AddJsonFile("appsettings.development.json", true)
+                                .Build();
+
+            var settings = new SignalRSettings();
+
+            configuration.Bind("SignalR", settings);
+
+            return settings;
+        }
+    }
+}
